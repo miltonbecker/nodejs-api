6da6ce8f490ecba4e8c820234f2c1a55b636062a
Milton Becker Junior <miltonbecker@gmail.com>
2017-03-13 11:54:34 -0300

    refactoring: calling the json method now

--- js/app.js
+++ js/app.js
@@ -14,8 +14,6 @@ app.get('/', function (req, res) {
 app.use('/node_modules', express.static(path.resolve(`${__dirname}/../node_modules`)));
 
 app.get('/api/getUserInfo/:name', function (req, res) {
-    res.set('Content-Type', 'application/json');
-
     let name = req.params.name;
     if (!name) {
         return res.sendStatus(400);
@@ -29,17 +27,16 @@ app.get('/api/getUserInfo/:name', function (req, res) {
             }
             res.send(result);
         }).catch(function (error) {
-            res.status(500).send(JSON.stringify({ Error: error.message }));
+            res.status(500).json({ Error: error.message });
         });
 });
 
 let jsonParser = parser.json({
     verify: function (req, res, buf, enc) {
-        res.set('Content-Type', 'application/json');
         try {
             JSON.parse(buf);
         } catch (error) {
-            res.status(400).send(JSON.stringify({ Error: error.message }));
+            res.status(400).json({ Error: error.message });
             throw Error(error);
         }
     }
@@ -55,22 +52,20 @@ app.post('/api/addOrUpdateUser', jsonParser, function (req, res) {
         .then(function (result) {
             switch (result) {
                 case constants.userAdded:
-                    res.send(JSON.stringify({ result: 'User added successfully' }));
+                    res.json({ result: 'User added successfully' });
                     break;
                 case constants.userUpdated:
-                    res.send(JSON.stringify({ result: 'User updated successfully' }));
+                    res.json({ result: 'User updated successfully' });
                     break;
                 default:
-                    res.status(400).send(JSON.stringify({ result: 'User was NOT added nor updated. Please check your data and try again.' }));
+                    res.status(400).json({ result: 'User was NOT added nor updated. Please check your data and try again.' });
             }
         }).catch(function (error) {
-            res.status(500).send(JSON.stringify({ Error: error.message }));
+            res.status(500).json({ Error: error.message });
         });
 });
 
 app.delete('/api/deleteUser/:name', function (req, res) {
-    res.set('Content-Type', 'application/json');
-
     let name = req.params.name;
     if (!name) {
         return res.sendStatus(400);
@@ -79,12 +74,12 @@ app.delete('/api/deleteUser/:name', function (req, res) {
     db.deleteUser(name)
         .then(function (result) {
             if (!result) {
-                res.status(400).send(JSON.stringify({ result: 'User was already gone' }));
+                res.status(400).json({ result: 'User was already gone' });
                 return;
             }
-            res.send(JSON.stringify({ result: 'User deleted successfully' }));
+            res.json({ result: 'User deleted successfully' });
         }).catch(function (error) {
-            res.status(500).send(JSON.stringify({ Error: error.message }));
+            res.status(500).json({ Error: error.message });
         });
 });
 
